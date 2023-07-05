94e5bfbd26c640e261e53591c49d394312edc10c
yctaii <yctai98@gmail.com>
2023-07-05 10:08:46 +0800

    sort_optimize

--- interview/src/sort_search/SelectSort.java
+++ interview/src/sort_search/SelectSort.java
@@ -8,21 +8,28 @@ import java.util.Arrays;
 public class SelectSort {
 
     public static void main(String[] args) {
-        int[] arr = { 1, 9, 6, 3, 5, 4, 7, 8, 2 };
+        int[] arr = {1, 9, 6, 3, 5, 4, 7, 8, 2};
         selectSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     private static void selectSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1 ; i++){
+        //每次选择最小的元素，如当前轮数的索引位置的元素交换
+        for (int i = 0; i < arr.length - 1; i++) {
+            //第0轮，循环结束则索引 0 是最小的
+            //第1轮，循环结束则索引 1 是最小的
+            //最后只需要选择索引为 arr.length - 2 的元素 会整体有序
             int minIndex = i;
-            for(int j = minIndex + 1; j < arr.length; j++){
-                if(arr[minIndex] > arr[j]){
+            for (int j = minIndex + 1; j < arr.length; j++) {  //最后一个元素也要比较
+                if (arr[minIndex] > arr[j]) {
                     minIndex = j;
                 }
             }
-            swap(arr , minIndex, i);
+            swap(arr, minIndex, i);
+            System.out.println("第" + i + "轮选择！" + Arrays.toString(arr));
         }
+
+
     }
 
     private static void swap(int[] arr, int minIndex, int i) {
--- interview/src/sort_search/insertSort.java
+++ interview/src/sort_search/insertSort.java
@@ -4,25 +4,33 @@ import java.util.Arrays;
 
 public class insertSort {
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 5, 2, 6, 7, 9, 4 };
+        int arr[] = {1, 3, 5, 8, 2, 6, 7, 9, 4};
         inserSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     private static void inserSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int t = arr[i];
-            int j = i - 1;
-            while (j >= 0) {
-                if (arr[j] > t) {
+        //假设前i - 1 个位置是有序的，待插入为第i个元素
+        //第一次假设索引0是有序，待插入为索引 ： 1 元素
+        //第二次假设索引0 - 1 是有序，待插入为索引 ：2 元素
+        //最后一个待插入元素为 arr.length - 1个元素
+        for(int i = 1; i < arr.length; i++){
+            int t = arr[i];     //待插入元素可能会被替换，先记录下来
+            int j = i - 1;      //有序的最后一个元素的索引，从它开始向前比较
+            while(j >= 0){
+                if(t < arr[j]){
+                    //如果待插入元素比前一个小，则向后移一位
                     arr[j + 1] = arr[j];
-                } else {
+                }else{
+                    //前面元素有序，当待插入元素比当前的大了
+                    //那它比前面所有元素都大，所以不需要移动
                     break;
                 }
-                j--;
+                j--;   //前面所有元素都要比较
             }
+            //当前的j是最后有序的前一位  j-- 所以j + 1 是待插入元素的位置
             arr[j + 1] = t;
-            System.out.println(Arrays.toString(arr));
         }
     }
+
 }
