682bd4abf80b61df20e6e95e982e6f7cf1705c46
yctaii <yctai98@gmail.com>
2023-07-16 16:30:56 +0800

    update selectSort

--- interview/src/sort_search/SelectSort.java
+++ interview/src/sort_search/SelectSort.java
@@ -21,9 +21,7 @@ public class SelectSort {
             //最后只需要选择索引为 arr.length - 2 的元素 会整体有序
             int minIndex = i;
             for (int j = minIndex + 1; j < arr.length; j++) {  //最后一个元素也要比较
-                if (arr[minIndex] > arr[j]) {
-                    minIndex = j;
-                }
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
             }
             swap(arr, minIndex, i);
             System.out.println("第" + i + "轮选择！" + Arrays.toString(arr));
